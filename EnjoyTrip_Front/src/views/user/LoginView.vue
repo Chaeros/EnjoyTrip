b619c00ae57558ef4cd274c8c56d19da7ce72561
Chaeros <chpark6851@koreatech.ac.kr>
2024-05-11 03:14:37 +0900

    [FE] Feat : 로그인 페이지 구현

--- EnjoyTrip_Front/src/views/user/LoginView.vue
+++ EnjoyTrip_Front/src/views/user/LoginView.vue
@@ -11,7 +11,6 @@ const loginInfo = ref({
 });
 
 const clickNormalLogin = () => {
-  console.log(loginInfo.value);
   normalLogin(
     loginInfo.value,
     (response) => {
@@ -32,47 +31,135 @@ const clickNormalLogin = () => {
     }
   );
 };
+
+const showPassword = (show) => {
+  const passwordInput = document.getElementById("exampleInputPassword1");
+  if (show) {
+    passwordInput.type = "text"; // Show password
+  } else {
+    passwordInput.type = "password"; // Hide password
+  }
+};
 </script>
 
 <template>
-  <div>
-    <form>
-      <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Email address</label>
-        <input
-          type="email"
-          class="form-control"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
-          v-model="loginInfo.email"
-        />
-        <div id="emailHelp" class="form-text">
-          We'll never share your email with anyone else.
+  <div class="loginPage">
+    <div class="loginBox">
+      <h1 class="login-title"><b>ChanGon</b></h1>
+      <form>
+        <div class="mb-3">
+          <input
+            type="email"
+            class="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="이메일"
+            v-model="loginInfo.email"
+          />
         </div>
-      </div>
-      <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input
-          type="password"
-          class="form-control"
-          id="exampleInputPassword1"
-          v-model="loginInfo.password"
-        />
-      </div>
-      <div class="mb-3 form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-      </div>
-      <button
-        type="submit"
-        class="btn btn-primary"
-        @click.prevent="clickNormalLogin"
-      >
-        로그인
-      </button>
-    </form>
-    <button @click="clickKakaoLogin">카카오 로그인</button>
+        <div class="mb-3" style="position: relative">
+          <input
+            type="password"
+            class="form-control"
+            id="exampleInputPassword1"
+            placeholder="비밀번호"
+            v-model="loginInfo.password"
+          />
+          <!-- 눈모양 아이콘 -->
+          <img
+            src="@/img/eye_icon.png"
+            alt="Show Password"
+            title="Show Password"
+            class="eye_icon"
+            @mousedown="showPassword(true)"
+            @mouseup="showPassword(false)"
+          />
+        </div>
+        <div class="mb-3 form-check">
+          <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+          <label class="form-check-label" for="exampleCheck1"
+            >아이디 저장</label
+          >
+        </div>
+        <button
+          type="submit"
+          class="btn btn-primary normal-login-btn"
+          @click.prevent="clickNormalLogin"
+        >
+          로그인
+        </button>
+        <div class="user-management">
+          <a>아이디(이메일)찾기</a> | <a>비밀번호 찾기</a> | <a>회원가입</a>
+        </div>
+        <div class="hr-with-text">
+          <hr />
+          <div class="text">간편 로그인</div>
+          <hr />
+        </div>
+      </form>
+      <img
+        class="social-login-btn"
+        @click="clickKakaoLogin"
+        src="@/img/kakao_login_medium_wide.png"
+      />
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.login-title {
+  margin-bottom: 50px;
+}
+.loginPage {
+  display: flex;
+  justify-content: center; /* Horizontally center the content */
+  align-items: center; /* Vertically center the content */
+  height: 100vh;
+}
+.loginBox {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.form-control {
+  width: 400px;
+}
+.eye_icon {
+  width: 25px;
+  position: absolute;
+  right: 10px;
+  top: 50%;
+  transform: translateY(-50%);
+  cursor: pointer;
+}
+.normal-login-btn {
+  width: 400px;
+  height: 50px;
+  font-weight: bold;
+}
+.user-management {
+  padding-top: 10px;
+  text-align: center;
+}
+.user-management a {
+  cursor: pointer; /* 마우스 모양을 선택 모양으로 변경 */
+}
+.hr-with-text {
+  display: flex;
+  align-items: center;
+}
+.hr-with-text hr {
+  flex-grow: 1;
+  border: 1px solid gray;
+}
+.hr-with-text .text {
+  margin: 10px 10px;
+  font-size: 12px;
+  color: gray;
+}
+.social-login-btn {
+  width: 400px;
+  height: 55px;
+  cursor: pointer;
+}
+</style>
