d97be99af80d99d826afa21b257e599e2beda640
Chaeros <chpark6851@koreatech.ac.kr>
2024-05-12 04:21:13 +0900

    [FE] Feat : 이미지 업로드/다운로드 예외처리
    - 이미지가 아닌 파일을 업로드하면, 서버로부터 400에러 받음
      이를 예외처리하여  이미지가 아닌 파일이 게시글에 올라가는 것을 방지함

--- EnjoyTrip_Front/src/views/review-board/ReviewWriteView.vue
+++ EnjoyTrip_Front/src/views/review-board/ReviewWriteView.vue
@@ -8,6 +8,8 @@
 
 <script setup>
 import { onMounted } from "vue";
+import axios from "axios";
+
 const { VITE_VUE_API_URL } = import.meta.env;
 
 onMounted(() => {
@@ -26,20 +28,26 @@ onMounted(() => {
           formData.append("file", file);
 
           // 서버에 이미지 업로드
-          const response = await fetch("http://localhost:8080/image/upload", {
-            method: "POST",
-            body: formData,
-          });
-
-          const data = await response.json();
-          const imageUrl = VITE_VUE_API_URL + data.url; // 서버에서 반환된 이미지 URL
-
-          // 에디터에 이미지 URL 삽입
-          document.execCommand(
-            "insertHTML",
-            false,
-            `<img src="${imageUrl}" />`
-          );
+          await axios
+            .post("http://localhost:8080/image/upload", formData, {
+              headers: {
+                "Content-Type": "multipart/form-data",
+              },
+            })
+            .then((response) => {
+              console.log(response.data.url);
+              const imageUrl = VITE_VUE_API_URL + response.data.url; // 서버에서 반환된 이미지 URL
+              // 에디터에 이미지 URL 삽입
+              document.execCommand(
+                "insertHTML",
+                false,
+                `<img src="${imageUrl}" />`
+              );
+              console.log(imageUrl);
+            })
+            .catch((error) => {
+              console.error("Error uploading image:", error);
+            });
         }
       }
     });
