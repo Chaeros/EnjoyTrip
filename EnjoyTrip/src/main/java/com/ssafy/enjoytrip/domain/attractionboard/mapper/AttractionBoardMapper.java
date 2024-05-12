28565f97844138b9ad44c67b5ad0f0cc2a7ecdb4
Chaeros <chpark6851@koreatech.ac.kr>
2024-05-12 12:35:43 +0900

    [BE] Feat : AttractionBoardMapper 개발

--- EnjoyTrip/src/main/java/com/ssafy/enjoytrip/domain/attractionboard/mapper/AttractionBoardMapper.java
+++ EnjoyTrip/src/main/java/com/ssafy/enjoytrip/domain/attractionboard/mapper/AttractionBoardMapper.java
@@ -1,7 +1,17 @@
 package com.ssafy.enjoytrip.domain.attractionboard.mapper;
 
+import com.ssafy.enjoytrip.domain.attractionboard.AttractionBoard;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface AttractionBoardMapper {
+    int insertAttractionBoard(AttractionBoard attractionBoard);
+    void updateAttractionBoard(AttractionBoard attractionBoard);
+    void deleteAttractionBoard(int id);
+    AttractionBoard findAttractionBoaard(int id);
+    List<AttractionBoard> findAttractionBoards();
+    List<AttractionBoard> findAttractinoBoardsByTitle(String title);
+    List<AttractionBoard> findAttractionBoardsByAttractionId(int attractionId);
 }
