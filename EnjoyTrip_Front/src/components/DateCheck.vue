9c6b475efd72212392ced8b12b5463bd3fa477b2
leeggon <138864974+leeggon@users.noreply.github.com>
2024-05-14 11:07:59 +0900

    [FE] Feat : 달력 구현

--- EnjoyTrip_Front/src/components/DateCheck.vue
+++ EnjoyTrip_Front/src/components/DateCheck.vue
@@ -1,9 +1,77 @@
-<script setup></script>
+<script setup>
+import { ref, defineProps, defineEmits } from 'vue';
+
+const props = defineProps({
+  showModal: Boolean,
+});
+
+const emit = defineEmits(['toggleModal', 'updateDates']);
+const startDate = ref('');
+const endDate = ref('');
+
+const toggleModal = () => {
+  emit('toggleModal');
+};
+
+const updateDates = () => {
+  emit('updateDates', { startDate: startDate.value, endDate: endDate.value });
+};
+</script>
 
 <template>
-  <div>
-    <h1>달력 만들자</h1>
+  <div
+    v-if="showModal"
+    class="modal show d-block"
+    tabindex="-1"
+    style="background-color: rgba(0, 0, 0, 0.5)"
+  >
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Select Travel Dates</h5>
+          <button type="button" class="btn-close" @click="toggleModal"></button>
+        </div>
+        <div class="modal-body">
+          <div class="mb-3">
+            <label for="startDate" class="form-label">Start Date</label>
+            <input
+              id="startDate"
+              type="date"
+              class="form-control"
+              v-model="startDate"
+              @change="updateDates"
+            />
+          </div>
+          <div class="mb-3">
+            <label for="endDate" class="form-label">End Date</label>
+            <input
+              id="endDate"
+              type="date"
+              class="form-control"
+              v-model="endDate"
+              @change="updateDates"
+            />
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" @click="toggleModal">
+            Close
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ EnjoyTrip_Front/src/views/plan/MakePlanView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const planId = route.params.planId;
+console.dir(route.params);
+console.dir(planId);
+</script>
+
+<template>
+  <div>
+    <h1>여행 계획 만들기</h1>
+  </div>
+</template>
+
+<style scoped></style>
